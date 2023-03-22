cee5685871995133517c24ef5401b5c9a7967624
Alexander Reinthal <alexander.reinthal@knowit.se>
2023-03-22 23:53:30 +0100

    add final parsing

--- datadrivet_hemnet_scraper/assets.py
+++ datadrivet_hemnet_scraper/assets.py
@@ -98,3 +98,54 @@ def hemnet_initial_search_links_webpages(hemnet_search_links: pd.DataFrame):
         "preview": df[["url", "date", "reason"]].head(20).to_markdown()
     }
     return Output(df, metadata=metadata)
+
+@asset
+def hemnet_search_basic_listing_data(hemnet_initial_search_links_webpages: pd.DataFrame):
+    """gets the pricing data from the final page"""
+    def get_basic_data_from_html(html_text: str) -> dict():
+        soup = BeautifulSoup(html_text, "html.parser")
+        price = soup.findAll("p", class_="property-info__price qa-property-price")[-1].get_text()
+        address = soup.find("h1", class_="qa-property-heading hcl-heading hcl-heading--size2").get_text()
+        location = soup.find("span", class_="property-address__area").get_text()
+        return {
+            "price": price,
+            "address": address,
+            "location": location
+        }
+    entries = []
+    for _, row in hemnet_initial_search_links_webpages.iterrows():
+        entry = get_basic_data_from_html(row["url"])
+        entry["url"] = row["url"]
+        entry["reason"] = row["reason"]
+        entry["date"] = row["date"]
+    df = pd.DataFrame(entries)
+    metadata = {
+        "num_records": len(df),
+        "preview": df[list(df.columns[:4])].head(20).to_markdown()
+    }
+    return Output(value=df, metadata=metadata)
+
+@asset
+def hemnet_search_detailed_listing_data(hemnet_initial_search_links_webpages: pd.DataFrame):
+    def get_detailed_data_from_html(html_text: str):
+        soup = BeautifulSoup(html_text, "html.parser")
+        s = soup.find("div", class_="property-attributes-table")
+        entry = {}
+        # this parses the table on the main page of the listing
+        # it includes some garbage data  which can be removed later
+        for k,v in zip(s('dt'), s('dd')):
+            entry[k] = v
+        return entry
+    entries = []
+    for _, row in hemnet_initial_search_links_webpages.iterrows():
+        entry = get_detailed_data_from_html(row["url"])
+        entry["url"] = row["url"]
+        entry["reason"] = row["reason"]
+        entry["date"] = row["date"]
+        entries.append(entry)
+    df = pd.DataFrame(entries)
+    metadata = {
+        "num_records": len(df),
+        "preview": df[list(df.columns[:4])].head(20).to_markdown()
+    }
+    return Output(value=df, metadata=metadata)
